00caad5c876541c97aed8fb611a67be011155bda
erin mclaughlin <emclaughlin2012@gmail.com>
2019-09-29 11:39:10 -0400

    Remove campaign-specific data from character model, so a character can be played in multiple campaigns.

--- CampaignManager.Domain/Character.cs
+++ CampaignManager.Domain/Character.cs
@@ -7,18 +7,6 @@ namespace CampaignManager.Domain
     {
         //-- Constructor
         public Character() { }
-        public Character(int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma,
-            Race race, Class cl)
-        {
-            Strength = strength;
-            Dexterity = dexterity;
-            Constitution = constitution;
-            Intelligence = intelligence;
-            Wisdom = wisdom;
-            Charisma = charisma;
-            Race = race;
-            Class = cl;
-        }
 
         //-- Public Properties
         public int Id { get; set; }
@@ -27,20 +15,13 @@ namespace CampaignManager.Domain
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        /* Abilitiy Scores */
+        /* Base Abilitiy Scores */
         public int Strength { get; set; }
         public int Dexterity { get; set; }
         public int Constitution { get; set; }
         public int Intelligence { get; set; }
         public int Wisdom { get; set; }
         public int Charisma { get; set; }
-
-        /* Points */
-        public int MaxHP { get; set; }
-        public int HP { get; set; }
-        public int Level { get; set; }
-        public int XP { get; set; }
-
 
         //-- Relationships
         [ForeignKey("UserId")]
---
+++ CampaignManager.Domain/Migrations/20190929153820_RemoveCampaignDataFromCharacter.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CampaignManager.Domain.Migrations
+{
+    public partial class RemoveCampaignDataFromCharacter : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HP",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "Level",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "MaxHP",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "XP",
+                table: "Characters");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "HP",
+                table: "Characters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Level",
+                table: "Characters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxHP",
+                table: "Characters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "XP",
+                table: "Characters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
